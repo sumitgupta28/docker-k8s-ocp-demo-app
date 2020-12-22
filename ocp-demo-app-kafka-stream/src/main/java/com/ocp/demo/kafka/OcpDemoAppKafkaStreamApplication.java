e46189d592d6920d37876ed3df1b70cddf62aaff
Sumit <sumitgupta28@gmail.com>
2020-12-21 23:07:38 -0500

    init commit ocp-demo-app-kafka-stream

---
+++ ocp-demo-app-kafka-stream/src/main/java/com/ocp/demo/kafka/OcpDemoAppKafkaStreamApplication.java
@@ -0,0 +1,13 @@
+package com.ocp.demo.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OcpDemoAppKafkaStreamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OcpDemoAppKafkaStreamApplication.class, args);
+	}
+
+}
---
+++ ocp-demo-app-kafka-stream/src/main/java/com/ocp/demo/kafka/controller/StreamController.java
@@ -0,0 +1,56 @@
+package com.ocp.demo.kafka.controller;
+
+import org.springframework.messaging.support.MessageBuilder;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ocp.demo.kafka.model.ChatMessage;
+import com.ocp.demo.kafka.producer.Producer;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
+@RestController
+@Api(description = "Set of endpoints for Sending Compexta dn Plain type of stream Messages.")
+public class StreamController {
+
+	private Producer producer;
+
+	public StreamController(Producer producer) {
+		super();
+		this.producer = producer;
+	}
+
+	// get the message as a complex type via HTTP, publish it to broker using spring cloud stream
+	@RequestMapping(value = "/sendMessage/complexType", method = RequestMethod.POST)
+	@ApiOperation("Get the message as a complex type via HTTP, publish it to broker using spring cloud stream")
+
+	public String publishMessageComplextType(@RequestBody ChatMessage payload) {
+
+		payload.setTime(System.currentTimeMillis());
+		producer.getMysource()
+			.output()
+			.send(MessageBuilder.withPayload(payload)
+				.setHeader("type", "chat")
+				.build());
+
+		return "success";
+	}
+
+	// get the String message via HTTP, publish it to broker using spring cloud stream
+	@RequestMapping(value = "/sendMessage/string", method = RequestMethod.POST)
+	@ApiOperation("Get the String message via HTTP, publish it to broker using spring cloud stream")
+	public String publishMessageString(@RequestBody String payload) {
+
+		// send message to channel
+		producer.getMysource()
+			.output()
+			.send(MessageBuilder.withPayload(payload)
+				.setHeader("type", "string")
+				.build());
+
+		return "success";
+	}
+}
